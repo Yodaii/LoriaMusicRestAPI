8b9b0661522abfd41cccc93d7f29f09c4f18f1e9
neo <arthurdu88@hotmail.com>
2017-01-11 14:54:14 +0100

    Ajout cross origin

--- src/main/java/com/mycompany/loriamusic/boundary/UserRepresentation.java
+++ src/main/java/com/mycompany/loriamusic/boundary/UserRepresentation.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,6 +55,7 @@ public class UserRepresentation {
     }
     
     //POST
+    @CrossOrigin(origins = "http://localhost:8081")
     @PostMapping
     public ResponseEntity<?> saveUser(@RequestBody User u){
         User saved = ur.save(u);
