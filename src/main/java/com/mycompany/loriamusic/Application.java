67a1a182eb1e03bb37196799f8ebaf4d23d30bd4
Yodaii <yohann.vaubourg@gmail.com>
2017-01-11 15:32:56 +0100

    affichage id track get

--- src/main/java/com/mycompany/loriamusic/Application.java
+++ src/main/java/com/mycompany/loriamusic/Application.java
@@ -5,14 +5,23 @@
  */
 package com.mycompany.loriamusic;
 
+import com.mycompany.loriamusic.entity.Track;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
+@Configuration
 @SpringBootApplication
-public class Application {
+public class Application extends RepositoryRestConfigurerAdapter {
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Track.class);
+    }
 }
